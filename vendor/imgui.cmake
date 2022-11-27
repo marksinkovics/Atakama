e0f957f106f433a070fe4a5a491d52e74d413a07
Mark Sinkovics <sinkovics.mark@gmail.com>
2022-11-26 20:58:30 -0500

    feat: use submodules
    - glm
    - imgui
    - stb
    - glfw

---
+++ vendor/imgui.cmake
@@ -0,0 +1,30 @@
+set(imgui_SOURCE_DIR_ ${CMAKE_CURRENT_SOURCE_DIR}/imgui)
+
+file(GLOB imgui_sources CONFIGURE_DEPENDS  "${imgui_SOURCE_DIR_}/*.cpp")
+file(GLOB imgui_impl CONFIGURE_DEPENDS
+    ${imgui_SOURCE_DIR_}/backends/imgui_impl_opengl3.cpp
+    ${imgui_SOURCE_DIR_}/backends/imgui_impl_glfw.h
+    ${imgui_SOURCE_DIR_}/backends/imgui_impl_glfw.cpp
+)
+
+add_library(imgui STATIC ${imgui_sources} ${imgui_impl})
+
+set_target_properties(imgui
+    PROPERTIES
+        CXX_STANDARD 20
+        CXX_STANDARD_REQUIRED OFF
+        CXX_EXTENSIONS OFF
+)
+
+target_include_directories(imgui
+    PUBLIC
+        $<BUILD_INTERFACE:${imgui_SOURCE_DIR_}>
+        $<BUILD_INTERFACE:${imgui_SOURCE_DIR_}>/backends
+)
+
+find_package(OpenGL REQUIRED)
+target_link_libraries(imgui
+    PUBLIC
+        glfw
+        ${OPENGL_LIBRARIES}
+)
