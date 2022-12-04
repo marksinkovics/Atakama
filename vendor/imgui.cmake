276f5c23c7923a128cbedc54bac0d2be3eb80843
Mark Sinkovics <sinkovics.mark@gmail.com>
2022-12-03 19:54:27 -0500

    feat:  build with Vulkan

--- vendor/imgui.cmake
+++ vendor/imgui.cmake
@@ -1,3 +1,6 @@
+find_package(OpenGL REQUIRED)
+find_package(Vulkan REQUIRED)
+
 set(imgui_SOURCE_DIR_ ${CMAKE_CURRENT_SOURCE_DIR}/imgui)
 
 file(GLOB imgui_sources CONFIGURE_DEPENDS  "${imgui_SOURCE_DIR_}/*.cpp")
@@ -5,6 +8,8 @@ file(GLOB imgui_impl CONFIGURE_DEPENDS
     ${imgui_SOURCE_DIR_}/backends/imgui_impl_opengl3.cpp
     ${imgui_SOURCE_DIR_}/backends/imgui_impl_glfw.h
     ${imgui_SOURCE_DIR_}/backends/imgui_impl_glfw.cpp
+    ${imgui_SOURCE_DIR_}/backends/imgui_impl_vulkan.cpp
+    ${imgui_SOURCE_DIR_}/backends/imgui_impl_vulkan.h
 )
 
 add_library(imgui STATIC ${imgui_sources} ${imgui_impl})
@@ -22,9 +27,10 @@ target_include_directories(imgui
         $<BUILD_INTERFACE:${imgui_SOURCE_DIR_}>/backends
 )
 
-find_package(OpenGL REQUIRED)
+
 target_link_libraries(imgui
     PUBLIC
         glfw
         ${OPENGL_LIBRARIES}
+        Vulkan::Vulkan
 )
