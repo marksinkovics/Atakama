933dc405d994f1d53df9329ff294f819085af921
Mark Sinkovics <sinkovics.mark@gmail.com>
2023-05-31 20:50:23 -0400

    fix: add missing files for the ImGui backend

--- vendor/imgui.cmake
+++ vendor/imgui.cmake
@@ -5,6 +5,8 @@ set(imgui_SOURCE_DIR_ ${CMAKE_CURRENT_SOURCE_DIR}/imgui)
 
 file(GLOB imgui_sources CONFIGURE_DEPENDS  "${imgui_SOURCE_DIR_}/*.cpp")
 file(GLOB imgui_impl CONFIGURE_DEPENDS
+    ${imgui_SOURCE_DIR_}/backends/imgui_impl_opengl3_loader.h
+    ${imgui_SOURCE_DIR_}/backends/imgui_impl_opengl3.h
     ${imgui_SOURCE_DIR_}/backends/imgui_impl_opengl3.cpp
     ${imgui_SOURCE_DIR_}/backends/imgui_impl_glfw.h
     ${imgui_SOURCE_DIR_}/backends/imgui_impl_glfw.cpp
